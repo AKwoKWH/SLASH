66a6f6b612b35a13a91a825da014a9eeea1d5ad6
Alex Kwok <alex.kwokwh@gmail.com>
2017-12-10 22:07:12 +0800

    regen

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,43 +5,25 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 import { ListPage } from '../pages/list/list';
-import { LoginPage } from '../pages/login/login';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
-
-import { AngularFireModule } from 'angularfire2';
-import { AngularFireAuthModule } from 'angularfire2/auth';
-
-
-const firebaseConfig = {
-    apiKey: "AIzaSyCdXjW-0EVsyvc9GYhVcc09w29tBvJ8I4k",
-    authDomain: "cumulonimbus-182209.firebaseapp.com",
-    databaseURL: "https://cumulonimbus-182209.firebaseio.com",
-    projectId: "cumulonimbus-182209",
-    storageBucket: "cumulonimbus-182209.appspot.com",
-    messagingSenderId: "242250640309"
-};
 
 @NgModule({
   declarations: [
     MyApp,
     HomePage,
-    ListPage,
-    LoginPage
+    ListPage
   ],
   imports: [
     BrowserModule,
     IonicModule.forRoot(MyApp),
-    AngularFireModule.initializeApp(firebaseConfig),
-    AngularFireAuthModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
     HomePage,
-    ListPage,
-    LoginPage
+    ListPage
   ],
   providers: [
     StatusBar,
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,11 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+
+import { AngularFireModule } from 'angularfire2';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
